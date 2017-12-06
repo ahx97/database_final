71f9dc3d11a489e5a318ae13c0b7fd63e863942d
Jack Murphy <jackmurphy569@gmail.com>
2017-12-05 22:02:14 -0600

    added the functions to post

--- db_interaction.php
+++ db_interaction.php
@@ -1,5 +1,11 @@
 <?php
   require('creds.php');
 
-  
+  function addToUsers($InsertName, $InsertPlateNumber, $InsertLotName, $InsertSpotNumber){
+    INSERT INTO users (Name, PlateNumber, LotName, SpotNumber) values ($InsertName, $InsertPlateNumber, $InsertLotNumber, $InsertSpotNumber);
+  }
+
+  function addToParkingLot($LotName, $spotNum, $price, $seller, $reserved){
+    INSERT INTO parkinglot (ParkingLotName, ParkingSpotNum, Price, SellerName, Reserved) values($LotName, $spotNum, $price, $seller, $reserved);
+  }
 ?>
